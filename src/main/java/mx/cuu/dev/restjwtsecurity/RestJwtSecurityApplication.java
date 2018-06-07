5ad1d8c043d192c8f93bd6cf9849152439391308
Julio Cesar Bolaños Palacios <jcesar.linux@gmail.com>
2018-06-07 13:36:40 -0500

    ADD - First commit. Projet created.

---
+++ src/main/java/mx/cuu/dev/restjwtsecurity/RestJwtSecurityApplication.java
@@ -0,0 +1,12 @@
+package mx.cuu.dev.restjwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJwtSecurityApplication.class, args);
+	}
+}
